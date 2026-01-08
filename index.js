e4fc111aa80fb0c2f401ab42c578fc007d7897bd
mjumbal <olafpastinof@outlook.com>
2026-01-08 07:40:48 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+document.getElementById("js-container").innerHTML = `
+<div id="js">
+<h2>JavaScript</h2>
+<p>
+This text comes from the <strong>index.js</strong> file,
+and is powered by javascript.
+</p> 
+<p>
+The way the page knows to run the javascript is that the 
+<strong>index.js</strong> file is included at the bottom
+of the <strong>index.html</strong> file.
+</p>
+</div>
+`;
